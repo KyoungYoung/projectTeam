258ec14f4e64cf3cab80507eaa0e8232d0917ffb
kky <kky9616tt@gmail.com>
2022-10-08 18:56:58 +0900

    test2

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ const port = 3500;
 app.listen(port, () => {
     console.log(`테스트 서버가 실행됩니다. http://localhost:${port}`);
 })
+
+
+// 시작 페이지 불러오기
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html');
 })
+
+
+// 로그인 페이지 불러오기
+app.get('/Interface/login.html',(req,res) => {
+    res.sendFile(__dirname + '/Interface/login.html')
+});
+
+
+// 시작 페이지 사진 가져오기
+app.use('/',(req,res) => {
+    fs.readFile('./asset/img/hansei_1.jpeg', (err, data) => {
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    })
+})
+
+
