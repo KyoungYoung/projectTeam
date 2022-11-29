6a2cb12b9fdf8a8672560f95ed0ba87e257b8c6e
kky <kky9616tt@gmail.com>
2022-11-29 18:48:40 +0900

    aa

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const portNum = 3000;
+
+app.listen(portNum,()=>{
+    console.log(`http://localhost:${portNum}`);
+});
+// css 연결
+app.use(express.static('public'));
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/login',(req,res) => {
+    res.sendFile(__dirname + '/login.html');
+})
+app.get('/join',(req,res) => {
+    res.sendFile(__dirname + '/join.html');
+
+})
