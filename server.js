2f8595a67af5c5521265c3530b8819b9296b2bf6
kky <kky9616tt@gmail.com>
2022-12-01 11:42:30 +0900

    tett

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const portNum = 3000;
+const passport = require('passport');
+const LogalStrategy = require('passport-local');
+const session = require('express-session');
+const { initialize } = require('passport');
+
+app.use(session({secret: '비밀코드',resave: true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(portNum,()=>{
     console.log(`http://localhost:${portNum}`);
@@ -8,6 +16,7 @@ app.listen(portNum,()=>{
 // css 연결
 app.use(express.static('public/css'));
 app.use(express.static('public/img'));
+app.use(express.static('public/js'));
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html')
