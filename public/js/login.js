f3ca7916cb054dc8ce0aa62708a22470e894709b
kky <kky9616tt@gmail.com>
2022-12-10 18:50:13 +0900

    end

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,16 @@
-const userIdInput = document.querySelector('.login_box li input[type=text]');
-const userPwInput = document.querySelector('.login_box li input[type=password]' )
-const userLoginBtn = document.querySelector('.login_box li button.btn_join');
+// const userIdInput = document.querySelector('.login_box li input[type=text]');
+// const userPwInput = document.querySelector('.login_box li input[type=password]' )
+// const userLoginBtn = document.querySelector('.login_box li button.btn_join');
 
 
-userIdInput.addEventListener('click',()=>{
-    let name = userIdInput.value;
-    console.log(name);
-})
-userPwInput.addEventListener('click',()=>{
-    alert('nice')
-})
+// // userIdInput.addEventListener('click',()=>{
+// //     let name = userIdInput.value;
+// //     console.log(name);
+// // })
+// // userPwInput.addEventListener('click',()=>{
+// //     alert('nice')
+// // })
 
-userLoginBtn.addEventListener('click',()=>{
-    location.href = '/join';
-})
+// userLoginBtn.addEventListener('click',()=>{
+//     location.href = '/join';
+// })
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
 const passport = require('passport');
-const LogalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
 const { initialize } = require('passport');
 
+// form 데이터 서버에 전송
+app.use(express.urlencoded({extended: true}));
+// 로그인
+app.use(session({secret: 'secret1234',resave: true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ejs
 app.set('view engine','ejs');
 app.set('views',__dirname + '/views')
 // db
 const MongoClient = require('mongodb').MongoClient;
 let db;
 
+// server, db 연결
 MongoClient.connect('mongodb+srv://admin1:kky1234@cluster0.3btbfxm.mongodb.net/graduate?retryWrites=true&w=majority', { useUnifiedTopology: true },(err, client) => {
 	if (err) return console.log(err);
 	db = client.db('graduate');
@@ -26,10 +34,7 @@ MongoClient.connect('mongodb+srv://admin1:kky1234@cluster0.3btbfxm.mongodb.net/g
         console.log(`http://localhost:${port}`);
     }); 
 });
-
-app.use(session({secret: '비밀코드',resave: true, saveUninitialized: false}));
-app.use(passport.initialize());
-app.use(passport.session());
+//
 
 
 // css 연결
@@ -37,6 +42,7 @@ app.use(express.static('public/css'));
 app.use(express.static('public/img'));
 app.use(express.static('public/js'));
 
+// get 요청
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
@@ -46,52 +52,65 @@ app.get('/login',(req,res) => {
 app.get('/join',(req,res) => {
     res.render('join.ejs');
 })
+
+// 로그인 후 아이디, 비번 검사
 app.post('/login',passport.authenticate('local', {failureRedirect : '/fail'}),(req,res)=>{
     res.redirect('/')
-});
-app.get('/mypage', function (req,res) {
-    res.render('mypage.ejs', {})
 })
-app.get('/mypage', loginTrue, function (req, res) { 
-    console.log(req.user); 
-    res.render('mypage.ejs', { 사용자: req.user }) 
-}) 
 
-function loginTrue(req, res, next) { 
-    if (req.user) { 
-        next() 
-    } 
-    else { 
-        res.send('로그인안하셨는데요?') 
-    } 
+// 회원가입
+app.post('/join', (req, res) => {
+    db.collection('login').insertOne({ id: req.body.id, pw: req.body.pw }, function (err, result) {
+      res.redirect('/')
+    })
+  })
+// 로그인 세션 값 가진 마이페이지
+app.get('/mypage',loginOk,(req,res)=>{
+    console.log(`마이페이지의 들어온 유저 정보: ${req.user}`);
+    res.render('mypage.ejs',{user:req.user})
+})
+
+function loginOk(req,res,next){
+    if(req.user){
+        next()
+    }else{
+        res.send('로그인 x 상태')
+    }
 }
 
 
+
+
+// 로그인
 passport.use(new LocalStrategy({
     usernameField: 'id', //사용자가 제출한 아이디가 어디 적혔는지
     passwordField: 'pw', //사용자가 제출한 비번이 어디 적혔는지
     session: true, //세션을 만들건지
     passReqToCallback: false, //아이디/비번말고 다른 정보검사가 필요한지
 }, function (inputID, inputPW, done) {
-    //console.log(inputID, inputPW);
+    console.log(inputID, inputPW);
     db.collection('login').findOne({ id: inputID }, function (err, result) {
         if (err) return done(err)
-
+        
         if (!result) return done(null, false, { message: '존재하지않는 아이디요' })
         if (inputPW == result.pw) {
             return done(null, result)
         } else {
-        return done(null, false, { message: '비번틀렸어요' })
+            return done(null, false, { message: '비번틀렸어요' })
         }
     })
 }));
 
-passport.serializeUser(function (user, done) {
-    done(null, user.id)
-    });
 
-passport.deserializeUser(function (yourID, done) {
-    db.collection('login').findOne({ id: yourID }, function (err, result) {
-        done(null, result)
+// 세션 데이터 저장 후 세션의 id정보를 쿠키로 보냄
+// id 이용해서 세션 저장시키는 코드(로그인 성공시 발동)
+passport.serializeUser(function (user, done) {
+    console.log('done');
+    done(null, user.id)
+});
+// 마이페이지 접속시 발동, 로그인한 유저의 개인정보를 db에서 찾는 역할
+passport.deserializeUser(function (userId, done) {
+    db.collection('login').findOne({id:userId},(err,result)=>{
+        done(null,result)
     })
 });
